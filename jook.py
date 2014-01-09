d4afd30f3680439620f04865c55de9345a07d70b
chandyland <cchan@cmu.edu>
2014-01-09 04:02:06 -0500

    auto redirect

--- jook.py
+++ jook.py
@@ -6,19 +6,29 @@ app = Flask(__name__)
 
 app_key = 'cl4r9w05dblwqhx'
 app_secret = '53br9rm0jljhire'
-
-flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
-
+flow = ''
 client = ''
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def main():
-    if request.method == 'POST':
-        key = request.form['key']
-        return newAuth(key)
-    else:
-        authorize_url = flow.start()
-        return render_template('index.html', auth=authorize_url)
+    global flow
+
+    redir_uri = url_for('authenticated', _external=True)
+    flow = dropbox.client.DropboxOAuth2Flow(app_key, app_secret, redir_uri, session, 'access_token')
+    authorize_url = flow.start()
+    return render_template('index.html', auth=authorize_url)
+
+@app.route('/authenticated/')
+def authenticated():
+    global client
+
+    try:
+        access_token, user_id, url_state = flow.finish(request.args)
+    except:
+        return redirect(url_for('main'))
+
+    client = dropbox.client.DropboxClient(access_token)
+    return redirect(url_for('player'))    
 
 def getLinks(m):
     songList = []
@@ -51,18 +61,7 @@ def player():
 
     return render_template('player.html', songs=links, jquery=url_for('static', filename='jquery-1.10.2.js'), styles=url_for('static', filename='styles.css'))
 
-
-def newAuth(key):
-    global client
-
-    try:
-        access_token, user_id = flow.finish(key)
-    except:
-        return "Invalid verification code."
-
-    client = dropbox.client.DropboxClient(access_token)
-
-    return redirect(url_for('.player'))
+app.secret_key = ''
 
 if __name__ == '__main__':
     app.run(debug=True)
